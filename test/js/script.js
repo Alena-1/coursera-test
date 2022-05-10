278679d86d5554e0305b0f2f4e0a2a4e31e6a79c
Alena-1 <alen4ik0307@ukr.net>
2022-05-10 19:32:23 +0300

    menu - collapsable-nav

---
+++ test/js/script.js
@@ -0,0 +1,14 @@
+var m1 = document.getElementById('m1').value;
+var m2 = document.getElementById('m2').value;
+
+function multi(x, y) {
+   /* return x * y;*/
+    console.log(x * y);
+}
+
+function divide(x, y) {
+    /*return x / y;*/
+    console.log(x / y);
+}
+
+
